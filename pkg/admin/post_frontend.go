c3d65cb5076ce4aabce780937f09f8e18e0f1a9f
muhwyndham <mwyndham@graveltechnology.com>
2023-08-17 21:38:20 +0700

    more styleeesssss and simplify cancel button

--- pkg/admin/post_frontend.go
+++ pkg/admin/post_frontend.go
@@ -17,14 +17,8 @@ func NewPostFrontend(g *echo.Group, repo models.PostRepository) {
 	}
 
 	g.GET("/posts/new", fe.PostsNew)
-	g.GET("/posts/new/cancel", fe.PostNewCancel)
 }
 
 func (fe *PostFrontend) PostsNew(c echo.Context) error {
 	return c.Render(http.StatusOK, "posts_new", nil)
-}
-
-func (fe *PostFrontend) PostNewCancel(c echo.Context) error {
-	c.Response().Header().Set("HX-Redirect", "/admin")
-	return c.Render(http.StatusOK, "admin_index", nil)
 }
