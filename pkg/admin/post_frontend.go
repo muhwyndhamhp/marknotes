f7aed515eaf81dd799c3773da3fe461e572a82f0
muhwyndham <mwyndham@graveltechnology.com>
2023-08-18 09:58:40 +0700

    lalala

--- pkg/admin/post_frontend.go
+++ pkg/admin/post_frontend.go
@@ -17,8 +17,27 @@ func NewPostFrontend(g *echo.Group, repo models.PostRepository) {
 	}
 
 	g.GET("/posts/new", fe.PostsNew)
+	g.POST("/posts/create", fe.PostCreate)
 }
 
 func (fe *PostFrontend) PostsNew(c echo.Context) error {
 	return c.Render(http.StatusOK, "posts_new", nil)
+}
+
+func (fe *PostFrontend) PostCreate(c echo.Context) error {
+	ctx := c.Request().Context()
+	post := models.Post{
+		Title:          c.FormValue("title"),
+		Content:        c.FormValue("content"),
+		EncodedContent: "",
+		Status:         models.Draft,
+	}
+
+	err := fe.repo.Upsert(ctx, &post)
+	if err != nil {
+		return err
+	}
+
+	c.Response().Header().Set("Hx-Redirect", "/admin")
+	return c.JSON(http.StatusOK, nil)
 }
