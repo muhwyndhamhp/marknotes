46fb77261980d952f12525b6ebad4bb129d7a69d
muhwyndham <mwyndham@graveltechnology.com>
2024-03-09 10:47:36 +0700

    add s3 client

---
+++ utils/cloudbucket/client.go
@@ -0,0 +1,38 @@
+package cloudbucket
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	goconf "github.com/muhwyndhamhp/marknotes/config"
+)
+
+func NewS3Client() *s3.Client {
+	accountId := goconf.Get(goconf.CF_ACCOUNT_ID)
+	accessKeyId := goconf.Get(goconf.CF_R2_ACCESS_KEY_ID)
+	accessKeySecret := goconf.Get(goconf.CF_R2_SECRET_ACCESS_KEY)
+
+	r2Resolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+		return aws.Endpoint{
+			URL: fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountId),
+		}, nil
+	})
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithEndpointResolverWithOptions(r2Resolver),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accessKeyId, accessKeySecret, "")),
+		config.WithRegion("auto"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := s3.NewFromConfig(cfg)
+
+	return client
+}
