c312f50bfd2f0e3fa497857d0cbd11f8915ad365
muhwyndham <mwyndham@graveltechnology.com>
2024-03-12 21:17:25 +0700

    add another allowed cors

--- utils/routing/setup.go
+++ utils/routing/setup.go
@@ -21,7 +21,7 @@ func SetupRouter(e *echo.Echo) {
 		rate.Limit(20),
 	)))
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"*://localhost:*", "*://www.github.com", "*://github.com", "*.fly.dev", "*://mwyndham.dev", "unpkg.com", "cdn.jsdelivr.net"},
+		AllowOrigins: []string{"*://localhost:*", "*://www.github.com", "*://github.com", "*.fly.dev", "*://mwyndham.dev", "unpkg.com", "cdn.jsdelivr.net", "static.cloudflare.com", "static.cloudflareinsights.com"},
 	}))
 
 	e.Validator = &validate.CustomValidator{
